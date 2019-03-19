a82199b1be012e3e5cd215b6052c336a85939b81
Monique <moniquevdh1@gmail.com>
2019-03-19 22:23:41 +0100

    Add challege 1 to page

---
+++ 3/laravel/public/js/clean.js
@@ -0,0 +1,26 @@
+var paragraph1 = `><Hello#521paddawan2==) Today---you0[]learn.,.-important.stuff`
+var paragraph2 = `*&Episode238I(& #$--THE33PHANTOM]}]MENACE[[`
+var paragraph3 = `&^%$Turmoil32<<has!-engulfed#2the:
+    ))-Galactic-3-0Republic.2€The8&&taxation
+    @)of#)#trade^@#*routes""{}to}}[outlying239{star
+    13-systems%%$is@%^in!235%dispute.23`
+var paragraph4 = `<,<Hoping><>#to81*resolve*--_the(-)matter*!*#
+    with??a??.blockade;\;of*&^*deadly??
+    battleships,@#%99the158*greedy%@*)Trade]}0-
+    Federation@#%3has@#@%3"stopped>?>all<#4
+    shipping9(8to(*&the&&$small$!@planet$#*
+    #@#T&of!?Naboo.$`
+var paragraph5 = `While38*the-Congress==+of#&&@the!#@
+    Republic$@:;endlessly?%#debates|}[
+    this235#%$^alarming_()8chain&)(@$of*&events,3
+    {[{--the0Supreme--=+Chancellor+09*has]
+    secretly@#]dispatched!!two%%$Jedi%:;
+    Knights,%the::guardians*&of+-
+    peace()and][3justice)&^}in@{the^}$
+    galaxy,8to##settle%@the@#%conflict....`    
+
+document.getElementById("paragraph1").innerHTML = paragraph1.replace(/[^a-zA-Z\r ]/g, " ");
+document.getElementById("paragraph2").innerHTML = paragraph2.replace(/[^a-zA-Z\r ]/g, " ");
+document.getElementById("paragraph3").innerHTML = paragraph3.replace(/[^a-zA-Z\r ]/g, " ");
+document.getElementById("paragraph4").innerHTML = paragraph4.replace(/[^a-zA-Z\r ]/g, " ");
+document.getElementById("paragraph5").innerHTML = paragraph5.replace(/[^a-zA-Z\r ]/g, " ");
